e40430d58823bb3d4fb51bfe61f58316ee09319f
Rocky <37962431+RockyR0ads@users.noreply.github.com>
2020-07-01 13:11:05 +1000

    0.0.1
    first commit

---
+++ reactapp/crypto-tracker-v1/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+import ParticlesClass from './components/Particles';
+import Particles from 'react-particles-js';
+import GetPrices from './components/callCryptoAPI'
+
+function App() {
+  return (
+    <div className="App">
+       
+       <header>
+         
+      </header>
+           
+      <ParticlesClass/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ reactapp/crypto-tracker-v1/src/components/callCryptoAPI.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+export class GetPrices extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            cryptos:[],
+            counter: 0
+        };
+
+    }
+
+    componentDidMount(){
+        axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,USDT,XRP,BCH,BSV,LTC,BNB,EOS,ADA&tsyms=USD&api_key={362a965976ce99a80e7e7955dbb0353c2d9db8df0ecd2817c32052ea3453b07f}')
+        .then(res => {
+            const cryptos = res.data
+            console.log(cryptos);
+            this.setState({cryptos: cryptos})
+        })
+    }
+
+    render() {
+        var counter = 1;
+        return (
+            
+            <div>
+                <table className="table table-striped table-dark" >
+                <thead>
+                    <tr id="tableHead"> 
+                    <th scope="col">Rank</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    
+                    </tr>
+                </thead>
+                <tbody>
+                {Object.keys(this.state.cryptos).map((key)=> (
+                
+                        
+                        <tr className="roar">
+                            <td id ="rank" scope="row">{counter++}</td>
+                            
+                                <td id ="name">{key}</td>
+                                <td id="price">{this.state.cryptos[key].USD}</td>
+
+                        </tr>
+                        
+                        
+                 
+                ))}
+                 </tbody>
+                 </table>
+
+            </div>
+        );
+        
+    }
+    
+}
+
+
+export default GetPrices
