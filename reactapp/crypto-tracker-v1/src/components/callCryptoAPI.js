6b998be994139eb91b4a608aa24dd957b7c8c503
Rocky <37962431+RockyR0ads@users.noreply.github.com>
2020-07-21 20:08:47 +1000

    0.0.3
    - added 1hr price change
    - added 24hr price change in money and percentage
    - added 24hr volume
    - added marketcap

--- reactapp/crypto-tracker-v1/src/components/callCryptoAPI.js
+++ reactapp/crypto-tracker-v1/src/components/callCryptoAPI.js
@@ -9,7 +9,9 @@ export class GetPrices extends Component {
         this.state = {
             cryptos:[],
             coins:[],
-            counter: 0
+            counter: 0,
+            changeColor: "#7FFF00",
+            changeValue: 0 
         };
 
         
@@ -28,35 +30,44 @@ export class GetPrices extends Component {
             this.setState({coins: coins})
         })
     }
+    
 
     render() {
         var counter = 1;
         var array = ["BTC","ETH"]
         var coins = "BTC"
-       
+        
         return (
             
             <div className="fullTable">
                 <table className="table table-striped table-dark" >
                 <thead>
                     <tr id="tableHead"> 
+
                     <th scope="col">Rank</th>
                     <th scope="col">Name</th>
                     <th scope="col">Price(USD)</th>
-                    
+                    <th scope="col">1Hr Change</th>
+                    <th scope="col">24Hr Change</th>
+                    <th scope="col">24Hr Volume</th>
+                    <th scope="col">Market Cap</th>
+
                     </tr>
                 </thead>
                 <tbody>
                 {Object.keys(this.state.coins).map((key)=> (
-                    
-                 
+                        
+                         
+                        
                         <tr className="roar">
                             <td id ="rank" scope="row">{counter++}</td>
                             
                                 <td id ="name">{key}</td>
                                 {<td id="price"><NumberFormat value={this.state.coins[key].USD.PRICE} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} /></td>}
-                                
-                                
+                                <td id ="1hrChange" style={{color:this.state.changeColor}}>{ this.state.coins[key].USD.CHANGEHOUR}</td>
+                                <td id ="24hrChange" >{this.state.coins[key].USD.CHANGE24HOUR} %{this.state.coins[key].USD.CHANGEPCT24HOUR}</td>
+                                <td id ="24Volume" >{this.state.coins[key].USD.VOLUME24HOUR}</td>
+                                <td id ="marketCap" >{this.state.coins[key].USD.MKTCAP}</td>
 
                         </tr>
                         
