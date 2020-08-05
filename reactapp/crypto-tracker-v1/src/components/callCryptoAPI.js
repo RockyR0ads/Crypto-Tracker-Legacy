7a54dc2f911a7ac0e3fdf13901c80b68ad1393ec
Will <post4wjm@gmail.com>
2020-08-05 17:59:29 +1000

    Implemented dynamic 1hr/24hr change + colouring

--- reactapp/crypto-tracker-v1/src/components/callCryptoAPI.js
+++ reactapp/crypto-tracker-v1/src/components/callCryptoAPI.js
@@ -1,123 +1,101 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import NumberFormat from 'react-number-format';
 
-export class GetPrices extends Component {
-    constructor(props){
-        super(props)
+const GetPrices = () => {
+  const [coins, setCoins] = useState([]);
+  const [tempArr, setTempArr] = useState([]);
+  const [tempArr2, setTempArr2] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  let counter = 1;
 
-        this.state = {
-            cryptos:[],
-            coins:[],
-            counter: 0,
-            changeColor: "#FFFFFF",
-            tempValue: 0 
-        };
-    }
+  useEffect(() => {
+    axios
+      .get(
+        'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,USDT,XRP,BCH,BSV,LTC,BNB,EOS,ADA&tsyms=USD&api_key={362a965976ce99a80e7e7955dbb0353c2d9db8df0ecd2817c32052ea3453b07f}'
+      )
+      .then((res) => {
+        const cryptos = res.data;
+        const tempCoins = cryptos['DISPLAY'];
+        setCoins(tempCoins);
+        setLoaded(true);
+      });
 
-    componentDidMount(){
-        axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,USDT,XRP,BCH,BSV,LTC,BNB,EOS,ADA&tsyms=USD&api_key={362a965976ce99a80e7e7955dbb0353c2d9db8df0ecd2817c32052ea3453b07f}')
-        .then(res => {
-            const cryptos = res.data
-            const coins = cryptos["DISPLAY"]
-            console.log(coins);
-            
-           // this.setState({cryptos: cryptos})
-            this.setState({coins: coins})
-        })
-        
-    }
+    if (loaded) colourChange();
+  }, [loaded]);
 
-    componentDidUpdate(value){
-        console.log(value);
-        var check = Math.sign(2)
-        if(value > 1){
-           // test();
-           // this.setState({changeColor: "#ff0000"})
-        }else{
-          //  this.setState({changeColor: "#00cc00"})
-        }
-        return(
-            value 
-        );
-    }
+  const colourChange = () => {
+    Object.keys(coins).map((key) => {
+      var check = Math.sign(coins[key].USD.CHANGEPCTHOUR);
+      var check2 = Math.sign(coins[key].USD.CHANGEPCT24HOUR);
 
-    checkChange=(value)=>{
-        
-       // console.log(value);
-        var check = Math.sign(2)
-        if(value == 1.65){
-           // test();
-            this.setState({changeColor: "#ff0000"})
-        }else{
-          //  this.setState({changeColor: "#00cc00"})
-        }
-        return(
-            value 
-        );
-        
-        
-    }
-    test(){
-        
-        
-           this.setState({changeColor: "#ff0000"})
-        
-        
-        
-    }
+      if (check === -1) {
+        const color = '#ff0000';
+        setTempArr((tempArr) => [...tempArr, color]);
+      } else {
+        const color = '#00cc00';
+        setTempArr((tempArr) => [...tempArr, color]);
+      }
 
-   
-    render() {
-        var counter = 1;
-        
-        return (
-            
-            <div className="fullTable">
-                <table className="table table-striped table-dark" >
-                <thead>
-                    <tr id="tableHead"> 
+      if (check2 === -1) {
+        const color = '#ff0000';
+        setTempArr2((tempArr2) => [...tempArr2, color]);
+      } else {
+        const color = '#00cc00';
+        setTempArr2((tempArr2) => [...tempArr2, color]);
+      }
+    });
+  };
 
-                    <th scope="col">Rank</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price(USD)</th>
-                    <th scope="col">1Hr Change</th>
-                    <th scope="col">24Hr Change</th>
-                    <th scope="col">24Hr Volume</th>
-                    <th scope="col">Market Cap</th>
+  return (
+    <div className='fullTable'>
+      <table className='table table-striped table-dark'>
+        <thead>
+          <tr id='tableHead'>
+            <th scope='col'>Rank</th>
+            <th scope='col'>Name</th>
+            <th scope='col'>Price(USD)</th>
+            <th scope='col'>1Hr Change</th>
+            <th scope='col'>24Hr Change</th>
+            <th scope='col'>24Hr Volume</th>
+            <th scope='col'>Market Cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(coins).map((key, index) => (
+            <tr className='roar'>
+              <td id='rank' scope='row'>
+                {counter++}
+              </td>
 
-                    </tr>
-                </thead>
-                <tbody>
-                {Object.keys(this.state.coins).map((key)=> (
-                        
-                        
-                        
-                        <tr className="roar">
-                            <td id ="rank" scope="row">{counter++}</td>
-                            
-                                <td id ="name">{key}</td>
-                                {<td id="price"><NumberFormat value={this.state.coins[key].USD.PRICE} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} /></td>}
-                                <td id ="1hrChange"  value={this.state.coins[key].USD.CHANGEPCTHOUR} style={{color:this.state.changeColor}}>{this.state.coins[key].USD.CHANGEPCTHOUR}%</td>
-                                <td id ="24hrChange" style={{color:this.state.changeColor}}>{this.componentDidUpdate(this.state.coins[key].USD.CHANGEPCT24HOUR)}%</td>
-                                <td id ="24Volume" >{this.state.coins[key].USD.VOLUME24HOUR}</td>
-                                <td id ="marketCap" >{this.state.coins[key].USD.MKTCAP}</td>
+              <td id='name'>{key}</td>
+              {
+                <td id='price'>
+                  <NumberFormat
+                    value={coins[key].USD.PRICE}
+                    displayType={'text'}
+                    decimalPrecision={2}
+                    thousandSeparator={true}
+                    prefix={'$'}
+                  />
+                </td>
+              }
+              <td id='1hrChange' style={{ color: tempArr[index] }}>
+                {coins[key].USD.CHANGEPCTHOUR}%
+              </td>
 
-                        </tr>
-                        
-                        
-                 
-                ))}
-                 </tbody>
-                 </table>
+              <td id='24hrChange' style={{ color: tempArr2[index] }}>
+                {coins[key].USD.CHANGEPCT24HOUR}%
+              </td>
 
-            </div>
-            
-        );
-        
-    }
-    
-}
+              <td id='24Volume'>{coins[key].USD.VOLUME24HOUR}</td>
+              <td id='marketCap'>{coins[key].USD.MKTCAP}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-
-export default GetPrices
+export default GetPrices;
